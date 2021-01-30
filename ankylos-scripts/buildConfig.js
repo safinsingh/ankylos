1468d5834444fd072c12d6a118d9ba2f49f41d79
Safin Singh <safin.singh@gmail.com>
2021-01-29 23:48:34 -0800

    fix: script typo

--- ankylos-scripts/buildConfig.js
+++ ankylos-scripts/buildConfig.js
@@ -5,7 +5,7 @@ const { promisify } = require('util')
 const exec = promisify(childProcess.exec).bind(childProcess)
 
 const buildConfig = (cfg) => {
-	if (existsSync(`ankylos-config-${cfg}/dist/`)) {
+	if (!existsSync(`ankylos-config-${cfg}/dist/`)) {
 		exec(`pnpm build --prefix ankylos-config-${cfg}`)
 	}
 }
